61d71462625317af0ca8cfb4fe22e7bdb953dd03
jeranders <jeranders10@gmail.com>
2015-03-17 09:27:44 +0100

    maj

--- index.php
+++ index.php
@@ -81,6 +81,7 @@ if (isset($_POST['send'])) {
               }else{
                 ?>
                 <li><a href="admin/index.php">Administration</a></li> 
+                <li><a href="logout.php">Logout</a></li> 
                 <?php
               } ?>
 
@@ -97,7 +98,7 @@ if (isset($_POST['send'])) {
           <p><a class="btn t-btn-green btn-lg" href="#" role="button">Learn more &raquo;</a></p>
         </div>
       </div>
-      <div class="row"><div class="col-md-4 col-md-offset-4"><?php echo flash();?></div></div>
+      <div class="col-md-4 col-md-offset-4"><?php echo flash();?></div>
       
       <!-- Button trigger modal -->
 
@@ -142,17 +143,17 @@ if (isset($_POST['send'])) {
           <div class="col-md-4">
             <h2>Contrôle de votre stock</h2>
             <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+            <p><a class="btn t-btn-default" href="#" role="button">View details &raquo;</a></p>
           </div>
           <div class="col-md-4">
             <h2>Carnet de clients</h2>
             <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+            <p><a class="btn t-btn-default" href="#" role="button">View details &raquo;</a></p>
           </div>
           <div class="col-md-4">
             <h2>Gestion des dépenses</h2>
             <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+            <p><a class="btn t-btn-default" href="#" role="button">View details &raquo;</a></p>
           </div>
         </div>
 
@@ -167,8 +168,11 @@ if (isset($_POST['send'])) {
 
           <hr>
 
+
+   
+
           <footer>
-            <p>&copy; Company 2015</p>
+            <p>&copy; E-asy Gest <?php echo date('Y'); ?></p>
           </footer>
         </div> <!-- /container -->
 
